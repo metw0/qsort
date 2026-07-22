599cb28f7e2bd6b30ab37cb196cc981ed024a7a9
metw0 <metwojustfunny@duck.com>
2026-07-23 02:18:38 +0300

    build: add cli11 lib

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,14 @@ set_version("1.0.0")
 set_languages("c++17", "c17")
 add_rules("mode.debug", "mode.release")
 
+add_requires("cli11")
 
 target("qsort")
     set_kind("binary")
     add_files("src/*.cpp")
     add_includedirs("include")
+
+    add_packages("cli11")
 
     add_syslinks("pthread", "dl")
     
