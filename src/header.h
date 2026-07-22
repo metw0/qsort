ba2b472ffc4907508a1715daec957c1bb014c8ee
metw0 <metwojustfunny@duck.com>
2026-07-23 02:17:33 +0300

    feat: add CLI logic

---
+++ src/header.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace ansi {
+    constexpr const char *BOLD_RED = "\033[1;31m";
+    constexpr const char *YELLOW = "\033[33m";
+    constexpr const char *RESET = "\033[0m";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
+#include <string>
+#include <unordered_map>
+#include <functional>
+#include "header.h"
 
 int main(int argc, char **argv) {
-    std::cout << "hello world!" << std::endl;
+    CLI::App qsort{"demo"};
+
+    std::string where_option;
+    qsort.add_option("-w, --where", where_option, "");
+    std::string type_option;
+    qsort.add_option("-t, --type", type_option, "");
+
+    CLI11_PARSE(qsort, argc, argv);
+
+    if(where_option.find("/") == std::string::npos) {
+        std::cout << ansi::BOLD_RED << "error: " << ansi::RESET << "/correct/path/ is acceptable, unknown path" << std::endl;
+        return 0; // return 1 gonna return 'unknown error', so it's 0 for correct returning
+    }
+
+    std::unordered_map<std::string, std::function<void()>> actions = {
+        {"date", []() {/* some code */}},
+        {"ext", []() {/* some code */}},
+        {"name", []() {/* some code */}}
+    };
+
+    auto it = actions.find(type_option);
+    if(it != actions.end()) {
+        it->second();
+    } else {
+        std::cout << ansi::BOLD_RED << "error: " << ansi::RESET << "types date, ext or name is acceptable, unknown type" << std::endl;
+        return 0; // return 1 gonna return 'unknown error', so it's 0 for correct returning
+    }
+
     return 0;
 }
