247d83134640676bf44690ee1986e7578bc63fe1
metw0 <metwojustfunny@duck.com>
2026-07-27 23:51:28 +0300

    refactor: switch to std::exit()

--- src/main.cpp
+++ src/main.cpp
@@ -10,42 +10,39 @@ int main(int argc, char **argv) {
     CLI::App qsort{"qsort - simple sorting programm for Linux"};
 
     std::string where_option;
-    qsort.add_option("-w, --where", where_option, "directory to sort");
+    qsort.add_option("-w, --where", where_option, "directory to sort")->required();
     std::string type_option;
-    qsort.add_option("-t, --type", type_option, "types to sort (date, ext or name)");
+    qsort.add_option("-t, --type", type_option, "types to sort (date, ext or name)")->required();
     std::string df_option; // d = distinctive feature
-    qsort.add_option("-d, --df", df_option, "distinctive feature to sort (like date or file extension)");
+    qsort.add_option("-d, --df", df_option, "distinctive feature to sort (like date or file extension)")->required();
 
     CLI11_PARSE(qsort, argc, argv);
 
     if(!files::is_directory(files::path(where_option))) {
         std::cerr << ansi::BOLD_RED << "error: " << ansi::RESET 
-                  << "path is not a directory, unknown path" << std::endl;
-        return 0; // return 1 gonna return 'unknown error', so it's 0 for correct returning
+                  << "path is not a directory" << std::endl;
+        std::exit(1); 
     }
-    if(df_option.empty()) {
-        std::cerr << ansi::BOLD_RED << "error: " << ansi::RESET 
-                  << "distinctive feature is empty, unknown distinctive feature";
-        return 0; // return 1 gonna return 'unknown error', so it's 0 for correct returning
-    } 
 
-    std::unordered_map<std::string, std::function<void()>> actions = {
-        {"name", [where_option, df_option]() {
-            if(sort::name_sort(where_option, df_option) == 1) return 0; 
-            // return 1 gonna return 'unknown error', so it's 0 for correct returning
+    std::unordered_map<std::string, std::function<int()>> actions = {
+        {"name", [where_option, df_option]() -> int {
+            return sort::name_sort(where_option, df_option);
         }},
-        {"ext", []() {/* some code */}},
-        {"date", []() {/* some code */}}
+        {"ext", [where_option, df_option]() -> int {
+            return sort::ext_sort(where_option, df_option);
+        }},
+        {"date", []() {/* some code */ return 0;}}
     };
 
     auto it = actions.find(type_option);
     if(it != actions.end()) {
-        it->second();
+        if(it->second() != 0) std::exit(1);
+        std::cout << "success" << std::endl;
     } else {
         std::cerr << ansi::BOLD_RED << "error: " << ansi::RESET 
-                  << "types date, ext or name is acceptable, unknown type" << std::endl;
-        return 0; // return 1 gonna return 'unknown error', so it's 0 for correct returning
+                  << "unknown type, use --help" << std::endl;
+        std::exit(1);
     }
 
-    return 0;
+    std::exit(0);
 }
